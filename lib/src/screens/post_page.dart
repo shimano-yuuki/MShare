96e4f4ef052f5c2d3059cdf1ef8039d87d96dd6c
simanoyuki <yuukireirei5@gmail.com>
2022-12-22 12:20:49 +0900

    image_pikerを使った画像の撮影、参照

--- lib/src/screens/post_page.dart
+++ lib/src/screens/post_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PostPage extends StatefulWidget {
   @override
@@ -6,14 +9,63 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future getImageFromCamera() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future _getImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("投稿画面"),
-        ),
-        body: Container(
-          child: Text("投稿画面"),
-        ));
+      appBar: AppBar(
+        title: Text("投稿画面"),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _image == null ? Text('画像はありません') : Image.file(_image!),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FloatingActionButton(
+                onPressed: getImageFromCamera,
+                child: Icon(Icons.add_a_photo),
+              ),
+              FloatingActionButton(
+                onPressed: _getImage,
+                child: Icon(Icons.image),
+              ),
+            ],
+          )
+        ],
+      )),
+    );
   }
 }
