2714cd3c3c88ce899ad3a023a551e93be3f85f55
simanoyuki <yuukireirei5@gmail.com>
2023-01-12 14:37:47 +0900

    ログアウトボタンをアカウント画面に追加

--- lib/src/screens/account.dart
+++ lib/src/screens/account.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../../auth.dart';
+
 class AccountScreen extends StatelessWidget {
 // 引数からユーザー情報を受け取れるようにする
   const AccountScreen(this.user);
@@ -9,7 +11,26 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('マイページ')),
+      appBar: AppBar(
+        title: const Text('マイページ'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              // ログアウト処理
+              // 内部で保持しているログイン情報等が初期化される
+              // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
+              await FirebaseAuth.instance.signOut();
+              // ログイン画面に遷移＋チャット画面を破棄
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return LoginPage();
+                }),
+              );
+            },
+          ),
+        ],
+      ),
       body: Center(
         // ユーザー情報を表示
         child: Text('ログイン情報：${user.email}'),
