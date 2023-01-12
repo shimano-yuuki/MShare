820162c0691d6800f972700a07eac74fa07a806f
simanoyuki <yuukireirei5@gmail.com>
2023-01-12 14:12:33 +0900

    プロフィール画面に自分のメールアドレスを表示できるようにする。

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 //全体共有
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/account.dart';
@@ -28,10 +29,11 @@ class NavigationWidget extends StatefulWidget {
 }
 
 class _NavigationWidgetState extends State<NavigationWidget> {
-  static const _screens = [
+  final _screens = [
     HomeScreen(),
     PostScreen(),
-    AccountScreen(),
+    if (FirebaseAuth.instance.currentUser != null)
+      AccountScreen(FirebaseAuth.instance.currentUser!),
     MessageScreen(),
   ];
 
--- lib/src/screens/account.dart
+++ lib/src/screens/account.dart
@@ -1,15 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
-  const AccountScreen({Key? key}) : super(key: key);
 // 引数からユーザー情報を受け取れるようにする
-
+  const AccountScreen(this.user);
+  // ユーザー情報
+  final User user;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('マイページ')),
-      body:
-          const Center(child: Text('マイページ', style: TextStyle(fontSize: 32.0))),
+      body: Center(
+        // ユーザー情報を表示
+        child: Text('ログイン情報：${user.email}'),
+      ),
     );
   }
 }
