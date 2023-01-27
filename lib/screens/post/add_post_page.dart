247ff7bba2e8eed76436a2359328a2dcbed4931a
simanoyuki <yuukireirei5@gmail.com>
2023-01-27 16:54:46 +0900

    投稿画面細かい調整

--- lib/screens/post/add_post_page.dart
+++ lib/screens/post/add_post_page.dart
@@ -4,16 +4,18 @@ import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class PostPage extends StatefulWidget {
   // 引数からユーザー情報を受け取る
-  PostPage(this.user);
+  const PostPage(this.user, {super.key});
   // ユーザー情報
   final User user;
 
   @override
+  // ignore: library_private_types_in_public_api
   _PostPageState createState() => _PostPageState();
 }
 
@@ -30,19 +32,19 @@ class _PostPageState extends State<PostPage> {
 
   ///ランダムに名前を取得する
   String randomString(int length) {
-    const _randomChars =
+    const randomChars =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const _charsLength = _randomChars.length;
+    const charsLength = randomChars.length;
 
-    final rand = new Random();
-    final codeUnits = new List.generate(
+    final rand = Random();
+    final codeUnits = List.generate(
       length,
       (index) {
-        final n = rand.nextInt(_charsLength);
-        return _randomChars.codeUnitAt(n);
+        final n = rand.nextInt(charsLength);
+        return randomChars.codeUnitAt(n);
       },
     );
-    return new String.fromCharCodes(codeUnits);
+    return String.fromCharCodes(codeUnits);
   }
 
   Future _getImage() async {
@@ -52,7 +54,9 @@ class _PostPageState extends State<PostPage> {
       if (pickedFile != null) {
         _image = File(pickedFile.path);
       } else {
-        print('No image selected.');
+        if (kDebugMode) {
+          print('No image selected.');
+        }
       }
     });
   }
@@ -62,11 +66,11 @@ class _PostPageState extends State<PostPage> {
       isLoading = true;
     });
 
-    String? imageURL = null;
+    String? imageURL;
     String rand = randomString(15);
 
     /// Firebase Cloud Storageにアップロード
-    String uploadName = '$rand';
+    String uploadName = rand;
     final storageRef =
         FirebaseStorage.instance.ref().child('users/$userID/$uploadName');
     final storedImage = await storageRef.putFile(_image!);
@@ -98,7 +102,7 @@ class _PostPageState extends State<PostPage> {
   @override
   Widget build(BuildContext context) {
     if (isLoading) {
-      return Scaffold(
+      return const Scaffold(
         body: Center(
           child: CircularProgressIndicator(),
         ),
@@ -106,29 +110,31 @@ class _PostPageState extends State<PostPage> {
     }
 
     return Scaffold(
-      appBar: AppBar(title: Text("投稿画面")),
+      appBar: AppBar(title: const Text("投稿画面")),
       body: Center(
           child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: 200.0, maxHeight: 200.0),
+              constraints:
+                  const BoxConstraints(maxWidth: 200.0, maxHeight: 200.0),
               child: Container(
-                  child:
-                      _image == null ? Text('画像はありません') : Image.file(_image!)),
+                  child: _image == null
+                      ? const Text('画像はありません')
+                      : Image.file(_image!)),
             ),
-            SizedBox(
+            const SizedBox(
               height: 30,
             ),
             FloatingActionButton(
               onPressed: _getImage,
-              child: Icon(Icons.image),
+              child: const Icon(Icons.image),
             ),
 
             // 投稿メッセージ入力
             TextFormField(
-              decoration: InputDecoration(labelText: '題名'),
+              decoration: const InputDecoration(labelText: '題名'),
               // 複数行のテキスト入力
               keyboardType: TextInputType.multiline,
               // 最大3行
@@ -140,7 +146,7 @@ class _PostPageState extends State<PostPage> {
               },
             ),
             TextFormField(
-              decoration: InputDecoration(labelText: '説明'),
+              decoration: const InputDecoration(labelText: '説明'),
               // 複数行のテキスト入力
               keyboardType: TextInputType.multiline,
               // 最大3行
@@ -152,9 +158,10 @@ class _PostPageState extends State<PostPage> {
               },
             ),
             const SizedBox(height: 8),
-            Container(
+            SizedBox(
               width: double.infinity,
-              child: ElevatedButton(child: Text('投稿'), onPressed: postdata),
+              child:
+                  ElevatedButton(onPressed: postdata, child: const Text('投稿')),
             ),
           ],
         ),
