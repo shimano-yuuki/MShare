8fb38ed01429477af60118580281f0dee71ddb22
simanoyuki <yuukireirei5@gmail.com>
2023-04-12 16:47:46 +0900

    違うユーザーが見た時のアイコンの表示

--- lib/screens/account/account_page.dart
+++ lib/screens/account/account_page.dart
@@ -43,6 +43,23 @@ class AccountScreen extends StatelessWidget {
                     );
                   },
                 ),
+              //同じユーザーが開かなかった場合
+              if (FirebaseAuth.instance.currentUser!.uid == uid)
+                IconButton(
+                  icon: const Icon(Icons.logout),
+                  onPressed: () async {
+                    // ログアウト処理
+                    // 内部で保持しているログイン情報等が初期化される
+                    // （現時点ではログアウト時はこの処理を呼び出せばOKと、思うぐらいで大丈夫です）
+                    await FirebaseAuth.instance.signOut();
+                    // ログイン画面に遷移＋チャット画面を破棄
+                    await Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) {
+                        return AuthPage();
+                      }),
+                    );
+                  },
+                ),
             ],
           ),
           body: Column(
